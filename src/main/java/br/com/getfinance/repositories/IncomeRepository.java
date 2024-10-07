3d4bd3b20b0ab58443085e9e74c0f10295c91987
juan.zeferino <juancarlosbatistazeferino@gmail.com>
2024-10-07 19:12:29 -0300

    refact: removing income domain

--- src/main/java/br/com/getfinance/repositories/IncomeRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.getfinance.repositories;
-
-import br.com.getfinance.models.Income;
-import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-
-public interface IncomeRepository extends JpaRepository<Income, Long>, JpaSpecificationExecutor<Income> {
-}
